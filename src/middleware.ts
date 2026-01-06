80f52f0fae0f8d3ae4ff4133dcc9c1cfa219f425
ondongo <gloireondongo1205@gmail.com>
2026-01-07 00:14:21 +0100

    Refactor authentication middleware to include custom authorization callback and maintain sign-in page configuration.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,22 @@
 import { withAuth } from 'next-auth/middleware';
 
-export default withAuth({
-  pages: {
-    signIn: '/login',
+export default withAuth(
+  function middleware(req) {
+    // Middleware qui s'exécute après vérification de l'auth
+    return;
   },
-});
+  {
+    callbacks: {
+      authorized: ({ token }) => {
+        // L'utilisateur est autorisé s'il a un token JWT
+        return !!token;
+      },
+    },
+    pages: {
+      signIn: '/login',
+    },
+  }
+);
 
 export const config = {
   matcher: [
